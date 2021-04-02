4094e7d85617ee89ed3325683e1a25ae92d66d2c
abdelghani <abdelghaniechlahi514@gmail.com>
2021-04-02 18:12:03 -0100

    d

---
+++ tests/Feature/authorizations/commentAutorizationTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Tests\Feature\authorization;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+use App\Models\User;
+use App\Models\Comment;
+
+class commentAutorizationTest extends TestCase
+{
+}
